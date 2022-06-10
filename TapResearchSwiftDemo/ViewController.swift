a9dbab39d2d733f0a5fd00ec659fcfd8052147b4
Jeroen Verbeek <>
2022-06-10 13:24:56 -0700

    Implement all TapResearch delegates.

--- TapResearchSwiftDemo/ViewController.swift
+++ TapResearchSwiftDemo/ViewController.swift
@@ -8,17 +8,20 @@
 import UIKit
 import TapResearchSDK
 
-class ViewController: UIViewController, TapResearchSurveyDelegate, TapResearchRewardDelegate, TapResearchPlacementDelegate {
+///---------------------------------------------------------------------------------------------
+class ViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var surveyButton: UIButton!
     
-    var tapResearchPlacement: TRPlacement?
-    
     var apiToken = "7d08c962b40ac7aa0cf83c4d376fa36f"
     var uniqueIdentifier = "Nascar"
-    
-    override func viewDidLoad() {
+    var placements: [String:TRPlacement] = [:]
+
+    //MARK: -
+
+    ///---------------------------------------------------------------------------------------------
+   override func viewDidLoad() {
         super.viewDidLoad()
         
         surveyButton.layer.cornerRadius = 10
@@ -28,13 +31,72 @@ class ViewController: UIViewController, TapResearchSurveyDelegate, TapResearchRe
         self.initSDK()
     }
     
-//SDK must be initialized with an api token and uniqueIdentifier
+    ///---------------------------------------------------------------------------------------------
+    /// SDK must be initialized with an api token and uniqueIdentifier as well as reward and placment delegates.
     func initSDK() {
+        
         TapResearch.initWithApiToken(apiToken, rewardDelegate: self, placementDelegate: self)
         TapResearch.setUniqueUserIdentifier(uniqueIdentifier)
     }
+
+    //MARK: - Actions and button handlers
     
-    func showSurveyAvailableButton() {
+    ///---------------------------------------------------------------------------------------------
+    /// present survey wall only when surveys are available.
+    @IBAction func handleSurveySelected() {
+        
+        if !placements.isEmpty {
+            if placements.values.count == 1 {
+                placements.values.first?.showSurveyWall(with: self)
+            }
+            else {
+                let alert: UIAlertController = UIAlertController(title: "TapResearch", message: "Choose an available placememt", preferredStyle: .alert)
+                for key: String in placements.keys {
+                    alert.addAction(UIAlertAction(title: key, style: .default) { (action: UIAlertAction) in
+                        if let title: String = action.title {
+                            self.placements[title]?.showSurveyWall(with: self)
+                        }
+                    })
+                }
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                self.present(alert, animated: true)
+            }
+        }
+    }
+
+}
+
+//MARK: - TapResearchSurveyDelegate
+
+///---------------------------------------------------------------------------------------------
+extension ViewController : TapResearchSurveyDelegate {
+    
+    ///---------------------------------------------------------------------------------------------
+    /// This delegate is called when the survey wall is opened.
+   func tapResearchSurveyWallOpened(with placement: TRPlacement) {
+        print("TapResearch: Modal opened with placement \(placement.placementIdentifier ?? "")")
+    }
+    
+    ///---------------------------------------------------------------------------------------------
+    /// This delegate is called when the survey wall is dismissed by the user.
+  func tapResearchSurveyWallDismissed(with placement: TRPlacement) {
+        print("TapResearch: Modal dismissed with placement \(placement.placementIdentifier ?? "")")
+    }
+
+}
+
+//MARK: - TapResearchPlacementDelegate
+
+///---------------------------------------------------------------------------------------------
+extension ViewController : TapResearchPlacementDelegate {
+    
+    ///---------------------------------------------------------------------------------------------
+    /// After the SDK is initialized, this delegate is called for each placement.
+    func placementReady(_ placement: TRPlacement) {
+        print("TapResearch: Placement Ready! \(placement.placementIdentifier ?? ""), wall \(placement.isSurveyWallAvailable), hot \(placement.hasHotSurvey), maxlen \(placement.maxSurveyLength), plcode \(placement.placementCode)")
+        
+        placements[placement.placementIdentifier] = placement
+        
         DispatchQueue.main.async {
             UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
                 self.surveyButton.alpha = 1.0
@@ -45,56 +107,80 @@ class ViewController: UIViewController, TapResearchSurveyDelegate, TapResearchRe
         }
     }
     
-    func showNotificationDialog(title: String, message: String) {
-        let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let alertOkAction: UIAlertAction = UIAlertAction(title: "Ok", style: .default) { (_: UIAlertAction) in }
-        alertController.addAction(alertOkAction)
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    //MARK:- Actions and button handlers
-    
-    // present survey wall when surveys are available
-    @IBAction func handleSurveySelected() {
-        tapResearchPlacement?.showSurveyWall(with: self)
-    }
-    
-    //MARK:- TapResearch Delegates
-    
-    // When the TapResearch modal is dismissed, this method is called with an array of TRReward to be used within your app
-    func tapResearchDidReceive(_ rewards: [TRReward]) {
-        print("Reward Received!")
-        
-        if let reward = rewards.first {
-            let message: String = String(format:"You have just received %lu %@ for your efforts.", reward.rewardAmount, reward.currencyName)
-            showNotificationDialog(title: "Congrats!", message: message)
-        }
-    }
-    
-    // After the SDK is initialized, this delegate is called for each placement
-    func placementReady(_ placement: TRPlacement) {
-        if tapResearchPlacement == nil && placement.isSurveyWallAvailable {
-            tapResearchPlacement = placement
-            showSurveyAvailableButton()
-        }
-    }
-    
-    // If the placement is not available for any reason, this delegate is called
+    ///---------------------------------------------------------------------------------------------
+    /// If the placement is not available for any reason, this delegate is called.
     func placementUnavailable(_ placementId: String) {
-        print("Placement Unavailable")
+        print("TapResearch: Placement Unavailable! \(placementId)")
+        
+        placements.removeValue(forKey: placementId)
+        
+        if placements.isEmpty {
+            DispatchQueue.main.async {
+                UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
+                    self.surveyButton.alpha = 0.0
+                } completion: { _ in
+                    self.surveyButton.alpha = 0.0
+                    self.activityIndicator.startAnimating()
+                }
+            }
+        }
     }
     
-    //MARK:- TapResearchSurveyDelegate
-    
-    // This delegate is called when the survey wall is opened
-    func tapResearchSurveyWallOpened(with placement: TRPlacement) {
-        print("Survey wall opened")
-    }
-    
-    // This delegate is called when the survey wall is dismissed
-    func tapResearchSurveyWallDismissed(with placement: TRPlacement) {
-        print("Survey wall dismissed")
-        tapResearchPlacement = nil
-    }
 }
 
+//MARK: - TapResearchRewardDelegate
+
+///---------------------------------------------------------------------------------------------
+extension ViewController : TapResearchRewardDelegate {
+    
+    ///---------------------------------------------------------------------------------------------
+    /// When there are rewards this delegate is called with an array of TRReward objects.
+    func tapResearchDidReceive(_ rewards: [TRReward]) {
+        print("Rewards (\(rewards.count) Received!)")
+        
+        var values: [AnyHashable : Any] = [:]
+        
+        rewards.forEach({
+            if values[$0.currencyName] != nil {
+                if var value = values[$0.currencyName] as? Int {
+                    value += $0.rewardAmount
+                    values[$0.currencyName] = $0.rewardAmount
+                }
+            } else {
+                values[$0.currencyName] = $0.rewardAmount
+            }
+        })
+        
+        var string = "You have just received "
+        var i = 1
+        let c = values.keys.count
+        
+        values.forEach {
+            if let currencyName: String = $0.key as? String {
+                string += "\($0.value) \(currencyName)"
+                switch i {
+                    case c - 1: string += " and "
+                    case c: string += " for your efforts!"
+                    default: string += ", "
+                }
+                i += 1
+            }
+        }
+        
+        presentAlert(title: "Congrats!", message: string)
+    }
+    
+    ///---------------------------------------------------------------------------------------------
+    func presentAlert(title: String, message: String) {
+        
+        DispatchQueue.main.async {
+            let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            controller.addAction(action)
+            if let sceneDelegate = UIApplication.shared.windows[0].windowScene?.delegate as? SceneDelegate {
+                sceneDelegate.window?.rootViewController?.present(controller, animated: true)
+            }
+        }
+    }
+    
+}
